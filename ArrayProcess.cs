2f9b94b3cba5d02e8fa9567e45a17e94fcac53db
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-02-06 10:55:19 +0900

    array process

---
+++ ArrayProcess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tec
+{
+    internal class ArrayProcess
+    {
+        public void Array_Select()
+        {
+            var v = new int[] { 100, 200, 300 };
+
+            var v2 = v.Select((x) => { return x + 15; });// {115, 215, 315}
+
+            //var v3 = v2.ToArray(); 
+
+            v2 = v.Select((x, i) => { return i * 10 + x; });//{100, 210, 320}
+
+            //v3 = v2.ToArray();
+        }
+    }
+}
