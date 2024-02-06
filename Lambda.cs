2e60cd101884f3f1d1b670a7a4ade962098102b0
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-02-06 15:18:45 +0900

    lambda

---
+++ Lambda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tec
+{
+    internal class Lambda
+    {
+        public void F()
+        {
+            //local func is better than delegate func<T>
+            int x3() => 3;
+            var x4 = () => 4;
+            var x5 = () => { return 5; };
+
+            var x1 = (int x) => x + 1;
+            int x2(int x) => x + 1;
+
+            int x6(int x, int y) => x + y;
+
+            var x7 = (int x, int y) =>
+            {
+                return x + y;
+            };
+
+            int x8(int x, int y)
+            {
+                return x + y;
+            };
+
+            int a = x1(1);
+            int b = x2(1);
+            int c = x3();
+            int d = x6(1, 2);
+            int e = x7(1, 2);
+            int f = x8(1, 2);
+        }
+    }
+}
