c35eb199db670e1d01448e8d7fc962532e294d8c
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-02-06 18:29:13 +0900

    tuple

---
+++ TupleProcess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tec
+{
+    internal class TupleProcess
+    {
+        public void F()
+        {
+            static (int, string) local()
+            {
+                return (1, "a");
+            }
+            var (v1, v2) = local();
+            v1 += 1;
+            v2 += 2;
+
+            var v = f();
+            var v3 = v.Item1;
+            var v4 = v.Item2;
+        }
+
+        private static Tuple<int, string> f()
+        {
+            return Tuple.Create(1, "a");
+        }
+    }
+}
