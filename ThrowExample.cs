f06f4d76e6aa5883f2b853e77ba069dbb29e69d1
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-02-06 10:34:42 +0900

    throw process

---
+++ ThrowExample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tec
+{
+    internal class ThrowExample
+    {
+        public void F()
+        {
+            try
+            {
+                int.Parse("s");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("parse error: ", ex); //message + except stack
+            }
+        }
+    }
+}
