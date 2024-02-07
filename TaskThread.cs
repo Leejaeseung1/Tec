a02d141942e9ac939e39c8fde192a4f24d87fa28
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-02-07 17:37:18 +0900

    callback task

--- TaskThread.cs
+++ TaskThread.cs
@@ -8,7 +8,6 @@ namespace Tec
             asyncFunc();
             asyncFunc2();
         }
-
         private async void asyncFunc()
         {
             var v = await getData(); //wait, run call thread next Run()
@@ -20,11 +19,31 @@ namespace Tec
             var v2 = await getData(); //wait, same
             Console.WriteLine(v + v2); //after 2000
         }
-
         private async Task<int> getData()
         {
             await Task.Delay(1000); //1000 time wait
             return 1;
+        }
+
+        private Action _action;
+
+        public async void Run2()
+        {
+            int value = await f(); //wait setResult TaskCompletionSource
+            //Console.WriteLine(value); print 1
+        }
+        private Task<int> f()
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            _action += () => { tcs.SetResult(1); };
+            f2(); //delay call callback func
+            return tcs.Task;
+        }
+        private async void f2()
+        {
+            await Task.Delay(1000);
+            _action();
         }
     }
 }
