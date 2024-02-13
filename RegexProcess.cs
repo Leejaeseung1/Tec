a7017571c8162a42292113ddef4012a95bd1cde4
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-02-13 15:12:28 +0900

    task, regex

---
+++ RegexProcess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tec
+{
+    internal partial class RegexProcess
+    {
+        public void F()
+        {
+            var str = Console.ReadLine();
+            var r = new Regex(@"\s");
+
+            if (r.IsMatch(str))
+            {
+                Console.WriteLine("no space");
+            }
+            else
+            {
+                //Console.WriteLine("good");
+            }
+
+            r = new Regex(@"[a-z|A-Z|0-9|_]+@[a-z|A-Z|0-9]+\.com");
+            if (r.IsMatch(str))
+            {
+                Console.WriteLine("good");
+            }
+            else
+            {
+                Console.WriteLine("no email");
+            }
+        }
+    }
+}
--- TaskThread.cs
+++ TaskThread.cs
@@ -3,20 +3,20 @@ namespace Tec
 {
     internal class TaskThread
     {
-        public void Run()
+        public void Run1() //use await
         {
             asyncFunc();
             asyncFunc2();
         }
         private async void asyncFunc()
         {
-            var v = await getData(); //wait, run call thread next Run()
-            Console.WriteLine(v); //run after 1000
+            var v = await getData(); //wait, run asyncFunc2(), play nextline after getData return
+            Console.WriteLine(v); //after 1000
         }
         private async void asyncFunc2()
         {
-            var v = await getData(); //wait, run call thread next Run()
-            var v2 = await getData(); //wait, same
+            var v = await getData(); //wait, run Run1(), play getData() after getData return
+            var v2 = await getData(); //wait, play nextline after getData return
             Console.WriteLine(v + v2); //after 2000
         }
         private async Task<int> getData()
@@ -27,9 +27,9 @@ namespace Tec
 
         private Action _action;
 
-        public async void Run2()
+        public async void Run2() //TaskCompletionSource: wait return
         {
-            int value = await f(); //wait setResult TaskCompletionSource
+            int value = await f(); //wait after setResult TaskCompletionSource
             //Console.WriteLine(value); print 1
         }
         private Task<int> f()
@@ -46,10 +46,13 @@ namespace Tec
             _action();
         }
 
-        public async void Run3()
+        public async void Run3() //task.when
         {
             var t1 = Task.Delay(1000);
             var t2 = f3();
+
+            t1.Start();
+            t2.Start();
 
             await Task.WhenAny(t1, t2);
             //t1, t2 any complete, do
@@ -59,9 +62,44 @@ namespace Tec
         }
         private async Task<int> f3()
         {
-            await Task.Delay(1000);
+            await Task.Delay(2000);
             return 1;
         }
 
+        CancellationTokenSource _cts = new CancellationTokenSource();
+        public async void Run4() //use token
+        {
+            var token = _cts.Token;
+            cancel();
+            var res = await Task.Run(run, token);
+
+            if (res == null)
+            {
+                Console.WriteLine("cancel");
+            }
+            else
+            {
+                Console.WriteLine("sum: " + res);
+            }
+        }
+        private async Task<int?> run()
+        {
+            int sum = 0;
+            for (int i = 0; i < 15; i++)
+            {
+                if (_cts.Token.IsCancellationRequested) //if cancel
+                {
+                    return null;
+                }
+                sum += i;
+                await Task.Delay(100);
+            }
+            return sum;
+        }
+        private async void cancel()
+        {
+            await Task.Delay(1000);
+            _cts.Cancel();
+        }
     }
 }
