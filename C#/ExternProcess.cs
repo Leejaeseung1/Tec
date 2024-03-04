1a4a9f813e3213b0a69dee2df0fabe2d7b8a6322
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-03-04 17:50:53 +0900

    use extern

---
+++ C#/ExternProcess.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Tec.C_
+{
+    internal class ExternProcess
+    {
+        [DllImport("User32.dll")] //win32
+        static extern int MessageBox(IntPtr h, string message, string title, int type);
+
+        public void F1()
+        {
+            var v = MessageBox(0, "content", "title", 0); //show popup message window
+            Console.WriteLine(v);
+        }
+    }
+}
